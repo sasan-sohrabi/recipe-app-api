752a81dd66da93a20c0b1d335862417da8b3e8b6
sasanpy <sasan.sp92@hotmail.com>
2021-10-20 00:36:27 +0330

    recipe app!!!

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,10 @@ RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
 
+RUN mkdir -p /vol/web/media
+RUN mkdir -p /vol/web/static
 RUN adduser -D user
-RUN chown user:user -R /app/
+RUN chown -R user:user /vol/
+RUN chmod -R 755 /vol/web
 USER user
+
--- app/app/settings.py
+++ app/app/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -130,3 +129,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
